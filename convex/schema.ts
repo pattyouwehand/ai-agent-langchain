7aa53e72427b243ed8a91e700234d3cc47db30ca
Patty Ouwehand <patty@g2g-online.nl>
2025-08-28 14:05:11 +0200

    Initiate model

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,17 +1,17 @@
-import { defineSchema, defineTable } from "convex/server"
-import { v } from "convex/values"
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
 
 export default defineSchema({
-  chats: defineTable ({
+  chats: defineTable({
     title: v.string(),
     userId: v.string(),
-    createAt: v.number()
+    createdAt: v.number(),
   }).index("by_user", ["userId"]),
 
-   messages: defineTable({
+  messages: defineTable({
     chatId: v.id("chats"),
     content: v.string(),
     role: v.union(v.literal("user"), v.literal("assistant")),
     createdAt: v.number(),
-  }).index("by_chat", ["chatId"])
- })
+  }).index("by_chat", ["chatId"]),
+});
